c52082c55af787a775b315763a73b0b365c4facd
lonelycat <24302010013@m.fudan.edu.cn>
2025-09-08 14:42:42 +0800

    更新 README.md 文件标题并添加 HelloWorld.cpp 示例程序

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# DataStr
+# Data Structures Course Repo
 
 a rep for 2025 autumn “数据结构" course
---
+++ lab0/src/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string hello = "Hello, World!";
+    cout << hello << endl;
+    return 0;
+}
